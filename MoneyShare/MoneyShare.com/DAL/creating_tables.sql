703641d1a81df8a6b2a581a222f3db0b92811d87
Mariia_Kolomiiets <49612039+Mariia-Braflowski@users.noreply.github.com>
2021-05-31 20:48:38 +0300

    creating table and scheme

---
+++ MoneyShare/MoneyShare.com/DAL/creating_tables.sql
@@ -0,0 +1,16 @@
+﻿create table records (
+    record_id serial PRIMARY KEY,
+    category_id int,
+    amount money,
+	date_record date,
+	FOREIGN KEY(category_id) REFERENCES categories(category_id)
+);
+
+create table categories (
+	category_id serial PRIMARY KEY,
+	title varchar(250),
+	color varchar(15),
+	icon varchar(110),
+	category_type int
+);
+
